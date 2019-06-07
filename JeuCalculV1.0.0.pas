c31cad46e59e2cec93bd78cd4490205a51dbc58c
LazareFortune <lazarefortune@gmail.com>
2019-06-07 13:08:11 +0100

    La premiere version du jeu en Pascal

---
+++ JeuCalculV1.0.0.pas
@@ -0,0 +1,27 @@
+program JeuCalcul;
+uses crt;
+const n=11;
+type
+	tableau=array[1..n] of integer;
+var
+	i,reponse,resultat,score,score2:integer;
+	tab:tableau;
+begin
+	writeln('Bonjour');
+	randomize;
+	for i:=1 to 
+	n do tab[i]:=random(100);
+	(*for i:=1 to (n-1) do write(tab[i],' ');*)
+	for i:=1 to (n-1) do
+		begin
+			writeln('entrez le resultat du calcul');
+			write(tab[i],' + ',tab[i+1], '=');
+			readln(reponse);
+			resultat:=(tab[i]+tab[i+1]);
+			if (reponse=resultat) then score:=score+1
+			else score2:=score2+1;
+			clrscr;
+		end;
+	writeln('Vous avez ',score,' bonnes reponses');
+	writeln('VOus avez ',score2,' mauvaises reponses');
+end.
